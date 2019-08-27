1017566d2f8ab3e6ab6a6a0a02742b7bb0a14a9b
单击开始 <2210563773@qq.com>
2019-08-27 10:32:08 +0800

    工具类

---
+++ src/test/java/com/lianghaojie/common/utils/StringUtilTest.java
@@ -0,0 +1,54 @@
+package com.lianghaojie.common.utils;
+
+import static org.junit.Assert.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.Test;
+
+import com.lianghaojie.entity.Person;
+
+public class StringUtilTest {
+	/*
+	 * @Test public void testHasLength() { }
+	 * 
+	 * @Test public void testHasText() { }
+	 * 
+	 * @Test public void testRandomChineseString() {
+	 * 
+	 * String string = StringUtil.randomChineseString(400);
+	 * System.out.println(string.length()); }
+	 * 
+	 * @Test public void testGenerateChineseName() { // String string =
+	 * StringUtil.generateChineseName(); // System.out.println(string);
+	 * StringUtil.hasLength(null, "abc");
+	 * 
+	 * }
+	 * 
+	 * @Test public void testPerson() { Calendar c = Calendar.getInstance();
+	 * c.set(2010, 0, 1);
+	 * 
+	 * for (int i = 0; i < 10000; i++) { Person person = new
+	 * Person(StringUtil.generateChineseName(), RandomUitl.random(1, 120),
+	 * 
+	 * StringUtil.randomChineseString(140), DateUtil.randomDate(c.getTime()));
+	 * 
+	 * System.out.println(person); }
+	 * 
+	 * }
+	 * 
+	 * @Test public void testDateUtil(){ Calendar c = Calendar.getInstance();
+	 * c.set(2010, 0, 1);
+	 * 
+	 * SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+	 * 
+	 * 
+	 * 
+	 * for(int i=0;i<1000;i++) { Date date = DateUtil.randomDate(c.getTime());
+	 * System.out.println(df.format(date)); }
+	 * 
+	 * }
+	 */
+}
